3f86c92474719968c71336d4d231e874523b863e
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-01 03:26:28 +0200

    add constructor to AbstractDao class

--- src/main/java/ba/unsa/etf/rpr/dal/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dal/AbstractDao.java
@@ -2,6 +2,29 @@ package ba.unsa.etf.rpr.dal;
 
 import ba.unsa.etf.rpr.beans.Idable;
 
-public abstract class AbstractDao<T extends Idable> implements Dao<T>{
-    
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+    private Connection connection;
+    private String tableName;
+
+    public AbstractDao(String tableName) {
+        try {
+            this.tableName = tableName;
+            Properties p = new Properties();
+            p.load(ClassLoader.getSystemResource("application.properties").openStream());
+            String url = p.getProperty("db.connection");
+            String username = p.getProperty("db.username");
+            String password = p.getProperty("db.password");
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (IOException | SQLException e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+        }
+
+    }
 }
