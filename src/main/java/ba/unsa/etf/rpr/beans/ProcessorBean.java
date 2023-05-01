a5f1304144c8a07ac32ab50c660143ae508413db
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-01 02:09:30 +0200

    override toString, equals and hashCode methods

--- src/main/java/ba/unsa/etf/rpr/beans/ProcessorBean.java
+++ src/main/java/ba/unsa/etf/rpr/beans/ProcessorBean.java
@@ -85,5 +85,21 @@ public final class ProcessorBean implements Idable{
         this.price = price;
     }
 
-    
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProcessorBean that = (ProcessorBean) o;
+        return id == that.id && name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
