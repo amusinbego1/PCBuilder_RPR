f9965b9fc1d1b32a9d0f81993768c576fc29d822
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-01 02:46:12 +0200

    add required fields to GraphicCardBean and correspoding getters and setters

--- src/main/java/ba/unsa/etf/rpr/beans/GraphicCardBean.java
+++ src/main/java/ba/unsa/etf/rpr/beans/GraphicCardBean.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.rpr.beans;
 
 public final class GraphicCardBean implements Idable{
     private int id;
+    private String name, manufacturer, desc, buyUrl, imgUrl;
+    double price;
 
     @Override
     public void setId(int id) {
@@ -12,4 +14,54 @@ public final class GraphicCardBean implements Idable{
     public int getId() {
         return id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getBuyUrl() {
+        return buyUrl;
+    }
+
+    public void setBuyUrl(String buyUrl) {
+        this.buyUrl = buyUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    
 }
