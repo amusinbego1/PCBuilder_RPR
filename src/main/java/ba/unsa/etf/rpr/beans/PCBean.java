866ff488d29ef87eb15adce7da7ffeaa44bc0692
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-01 02:54:48 +0200

    add required fields and corresponding setters and getters to PCBean class

--- src/main/java/ba/unsa/etf/rpr/beans/PCBean.java
+++ src/main/java/ba/unsa/etf/rpr/beans/PCBean.java
@@ -1,7 +1,8 @@
 package ba.unsa.etf.rpr.beans;
 
 public final class PCBean implements Idable{
-    private int id;
+    private int id, processorId, ramId, graphicCardId;
+    private double price;
 
     @Override
     public void setId(int id) {
@@ -12,4 +13,37 @@ public final class PCBean implements Idable{
     public int getId() {
         return id;
     }
+
+    public int getProcessorId() {
+        return processorId;
+    }
+
+    public void setProcessorId(int processorId) {
+        this.processorId = processorId;
+    }
+
+    public int getRamId() {
+        return ramId;
+    }
+
+    public void setRamId(int ramId) {
+        this.ramId = ramId;
+    }
+
+    public int getGraphicCardId() {
+        return graphicCardId;
+    }
+
+    public void setGraphicCardId(int graphicCardId) {
+        this.graphicCardId = graphicCardId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
 }
