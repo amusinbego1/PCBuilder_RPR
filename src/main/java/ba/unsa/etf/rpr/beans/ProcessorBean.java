f1c25bf1eb29d233f95dd8122046e76fa13cc538
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-01 02:03:37 +0200

    add constructors in ProcessorBean

--- src/main/java/ba/unsa/etf/rpr/beans/ProcessorBean.java
+++ src/main/java/ba/unsa/etf/rpr/beans/ProcessorBean.java
@@ -5,13 +5,36 @@ public class ProcessorBean implements Idable{
     private String name, manufacturer, desc, buy_url, img_url;
     double price;
 
+    public ProcessorBean(int id, String name, String manufacturer, String desc, String buy_url, String img_url, double price) {
+        this.id = id;
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.desc = desc;
+        this.buy_url = buy_url;
+        this.img_url = img_url;
+        this.price = price;
+    }
+
+    public ProcessorBean() {
+        id = 0;
+        price = 0;
+        name = "";
+        manufacturer = "";
+        desc = "";
+        buy_url = "";
+        img_url = "";
+    }
+
     @Override
     public void setId(int id) {
-
+        this.id = id;
     }
 
     @Override
     public int getId() {
-        return 0;
+        return id;
     }
+
+    
+
 }
