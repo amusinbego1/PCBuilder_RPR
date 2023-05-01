9ede205bd82d77030006821443850cb1563bf15a
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-01 02:00:36 +0200

    add required fields to ProcessorBean

--- src/main/java/ba/unsa/etf/rpr/beans/ProcessorBean.java
+++ src/main/java/ba/unsa/etf/rpr/beans/ProcessorBean.java
@@ -2,7 +2,8 @@ package ba.unsa.etf.rpr.beans;
 
 public class ProcessorBean implements Idable{
     private int id;
-    
+    private String name, manufacturer, desc, buy_url, img_url;
+    double price;
 
     @Override
     public void setId(int id) {
