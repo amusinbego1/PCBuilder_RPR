3e44f744a5ab325cf6eec4b9c6a9b37d87cf577a
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-01 02:21:21 +0200

    add required fields to RamBean and corresponding getters and setters

--- src/main/java/ba/unsa/etf/rpr/beans/RamBean.java
+++ src/main/java/ba/unsa/etf/rpr/beans/RamBean.java
@@ -2,6 +2,9 @@ package ba.unsa.etf.rpr.beans;
 
 public final class RamBean  implements Idable{
     private int id;
+    private String name, manufacturer, desc, buyUrl, imgUrl;
+    double price;
+
 
     @Override
     public void setId(int id) {
@@ -11,5 +14,53 @@ public final class RamBean  implements Idable{
     @Override
     public int getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getBuyUrl() {
+        return buyUrl;
+    }
+
+    public void setBuyUrl(String buyUrl) {
+        this.buyUrl = buyUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
