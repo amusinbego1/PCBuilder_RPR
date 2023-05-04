8ec59c2df032a9843b491db153b8a862f158c4c2
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-04 14:29:45 +0200

    add abstract methods to AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dal/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dal/AbstractDao.java
@@ -1,11 +1,15 @@
 package ba.unsa.etf.rpr.dal;
 
 import ba.unsa.etf.rpr.beans.Idable;
+import ba.unsa.etf.rpr.exceptions.PCBuilderException;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public abstract class AbstractDao<T extends Idable> implements Dao<T> {
@@ -15,16 +19,20 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T> {
     public AbstractDao(String tableName) {
         try {
             this.tableName = tableName;
-            Properties p = new Properties();
-            p.load(ClassLoader.getSystemResource("application.properties").openStream());
-            String url = p.getProperty("db.connection");
-            String username = p.getProperty("db.username");
-            String password = p.getProperty("db.password");
+            Properties properties = new Properties();
+            properties.load(ClassLoader.getSystemResource("application.properties").openStream());
+            String url = properties.getProperty("db.connection");
+            String username = properties.getProperty("db.username");
+            String password = properties.getProperty("db.password");
             this.connection = DriverManager.getConnection(url, username, password);
         } catch (IOException | SQLException e) {
             System.out.println("Connection failed");
             e.printStackTrace();
         }
-
     }
+
+    public abstract T rowToObject(ResultSet resultSet) throws PCBuilderException;
+    public abstract Map<String, Object> objectToRow(T object);
+
+    
 }
