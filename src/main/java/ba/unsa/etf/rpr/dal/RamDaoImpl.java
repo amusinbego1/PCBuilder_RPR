4eb985ec571010f605132c01740c14ce5b42c586
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-08 12:38:48 +0200

    make RamDaoImpl class

---
+++ src/main/java/ba/unsa/etf/rpr/dal/RamDaoImpl.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr.dal;
+
+import ba.unsa.etf.rpr.beans.PCComponent;
+import ba.unsa.etf.rpr.beans.RamBean;
+import ba.unsa.etf.rpr.exceptions.PCBuilderException;
+
+import java.sql.ResultSet;
+
+public class RamDaoImpl extends AbstractPCComponentDao{
+
+    private static RamDaoImpl instance = null;
+
+    private RamDaoImpl(String tableName) {
+        super(tableName);
+    }
+    public RamDaoImpl getInstance(){
+        if(instance == null) instance = new RamDaoImpl("rams");
+        return instance;
+    }
+
+    @Override
+    public PCComponent rowToObject(ResultSet resultSet) throws PCBuilderException {
+        try{
+            PCComponent ram = new RamBean();
+            return rowToGivenObject(ram, resultSet);
+        } catch(Exception e){
+            throw new PCBuilderException(e.getMessage(), e);
+        }
+    }
+}
