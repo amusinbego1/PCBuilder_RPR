8bf9d051eb756d050db44cefdce1eeb1ff7a9540
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-05-15 20:07:50 +0200

    make new implementation of PCBeanManager

--- src/main/java/ba/unsa/etf/rpr/business/PCBeanManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/PCBeanManager.java
@@ -3,12 +3,24 @@ package ba.unsa.etf.rpr.business;
 import ba.unsa.etf.rpr.beans.decorator.pc.PC;
 import ba.unsa.etf.rpr.dal.Dao;
 import ba.unsa.etf.rpr.dal.DaoFactory;
+import ba.unsa.etf.rpr.dal.pc.PCDao;
 import ba.unsa.etf.rpr.dal.pc.PCDaoImpl;
 
 public class PCBeanManager extends AbstractPCManager{
+
+    private static PCDao pcDao = DaoFactory.getPCDao(PCDaoImpl.getInstance());
+
+    public PCBeanManager(PCDao dao) {
+        pcDao = dao;
+    }
+
+    public PCBeanManager(){
+
+    }
+
     @Override
     protected Dao<PC> getDao() {
-        return DaoFactory.getPCDao(PCDaoImpl.getInstance());
+        return pcDao;
     }
 
 }
