2b54e7ba3b614c6b61079413896220b34b4c4f34
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-06-17 17:47:32 +0200

    fix surefire plugin issue

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module ba.unsa.etf.rpr {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires java.sql;
+    requires org.apache.commons.lang3;
+
+    exports ba.unsa.etf.rpr;
+}
