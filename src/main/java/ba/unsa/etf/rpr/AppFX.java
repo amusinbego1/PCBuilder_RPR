1e3774a34e9a9f4b08c049ab29d982266d2c657a
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-06-21 10:27:30 +0200

    add progressBar and stampmark

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -12,8 +12,11 @@ public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AppFX.class.getResource("/ba.unsa.etf.rpr.fxml/home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 540);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 600);
         stage.setTitle("PC Builder");
+        stage.setMinHeight(600);
+        stage.setMinWidth(700);
+
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/ba/unsa/etf/rpr/controllers/Home.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Home.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
 public class Home {
+
+    @FXML
+    public Label statusCode;
+    @FXML
+    public ProgressBar progressBar;
 }
