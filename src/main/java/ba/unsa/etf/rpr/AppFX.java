87fcdabb3d22e9e4b0f4ea566c2af9fa0ac22383
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-06-21 10:51:22 +0200

    add icon to home

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -3,20 +3,22 @@ package ba.unsa.etf.rpr;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AppFX.class.getResource("/ba.unsa.etf.rpr.fxml/home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AppFX.class.getResource("/fxml/home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 600);
         stage.setTitle("PC Builder");
         stage.setMinHeight(600);
         stage.setMinWidth(700);
-
+        stage.getIcons().add(new Image(Objects.requireNonNull(AppFX.class.getResource("/icons/builder.png")).openStream()));
         stage.setScene(scene);
         stage.show();
     }
