6a246b3a0ef07a50e4f2672eb1e1bf88902f7081
Amer Musinbegovic <amusinbego1@etf.unsa.ba>
2023-06-21 11:44:58 +0200

    add buttons and icons to right side of border pane

--- src/main/java/ba/unsa/etf/rpr/controllers/Home.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Home.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
@@ -13,4 +14,12 @@ public class Home {
     public ProgressBar progressBar;
     @FXML
     public TextField searchField;
+    @FXML
+    public Button pcButton;
+    @FXML
+    public Button processorButton;
+    @FXML
+    public Button graphcardButton;
+    @FXML
+    public Button ramButton;
 }
